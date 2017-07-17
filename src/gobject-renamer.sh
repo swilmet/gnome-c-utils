ac5f2afaa96fd3ffb780465eb76ea78cb445de6d
Sébastien Wilmet <swilmet@gnome.org>
2017-07-17 14:32:53 +0200

    gobject-renamer: variant

--- src/gobject-renamer.sh
+++ src/gobject-renamer.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-old_classname_camel=TabList
+old_classname_camel=MenuFactory
 old_classname_upper=$(gcu-case-converter --to-uppercase "$old_classname_camel")
 old_classname_lower=$(gcu-case-converter --to-lowercase "$old_classname_camel")
 
-new_classname_camel=TabGroup
+new_classname_camel=Factory
 new_classname_upper=$(gcu-case-converter --to-uppercase "$new_classname_camel")
 new_classname_lower=$(gcu-case-converter --to-lowercase "$new_classname_camel")
 
-ls *.[ch] | parallel --will-cite gcu-lineup-substitution "$old_classname_upper" "$new_classname_upper"
-ls *.[ch] | parallel --will-cite gcu-lineup-substitution "$old_classname_camel" "$new_classname_camel"
-ls *.[ch] | parallel --will-cite gcu-lineup-substitution "$old_classname_lower" "$new_classname_lower"
+filename=amtk-factory
+
+ls $filename.[ch] | parallel --will-cite gcu-lineup-substitution "$old_classname_upper" "$new_classname_upper"
+ls $filename.[ch] | parallel --will-cite gcu-lineup-substitution "$old_classname_camel" "$new_classname_camel"
+ls $filename.[ch] | parallel --will-cite gcu-lineup-substitution "$old_classname_lower" "$new_classname_lower"
