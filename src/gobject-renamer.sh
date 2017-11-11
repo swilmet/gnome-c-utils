95668bf8695a3f6573b43d1e60241e068613e1bc
Sébastien Wilmet <swilmet@gnome.org>
2017-11-11 18:34:55 +0100

    gobject-renamer: make it mergeable, add TODO list
    Not tested, I'll test it the next time that I need to rename a GObject.

--- src/gobject-renamer.sh
+++ src/gobject-renamer.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-old_classname_camel=MenuFactory
+# TODO
+# - Take also the namespace, and substitute:
+# "${old_namespace_upper}_IS_${old_classname_upper}"
+# "${old_namespace_upper}_TYPE_${old_classname_upper}"
+#
+# - Take input arguments from the command line, check the args, print usage,
+#   rename the script to gcu-gobject-renamer and install it.
+
+filename=$1
+
+old_classname_camel=Foo
 old_classname_upper=$(gcu-case-converter --to-uppercase "$old_classname_camel")
 old_classname_lower=$(gcu-case-converter --to-lowercase "$old_classname_camel")
 
-new_classname_camel=Factory
+new_classname_camel=Bar
 new_classname_upper=$(gcu-case-converter --to-uppercase "$new_classname_camel")
 new_classname_lower=$(gcu-case-converter --to-lowercase "$new_classname_camel")
 
-filename=amtk-factory
-
-ls $filename.[ch] | parallel --will-cite gcu-lineup-substitution "$old_classname_upper" "$new_classname_upper"
-ls $filename.[ch] | parallel --will-cite gcu-lineup-substitution "$old_classname_camel" "$new_classname_camel"
-ls $filename.[ch] | parallel --will-cite gcu-lineup-substitution "$old_classname_lower" "$new_classname_lower"
+gcu-lineup-substitution "$old_classname_upper" "$new_classname_upper" "$filename"
+gcu-lineup-substitution "$old_classname_camel" "$new_classname_camel" "$filename"
+gcu-lineup-substitution "$old_classname_lower" "$new_classname_lower" "$filename"
