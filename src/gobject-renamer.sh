c46d62709710ea20221ee759bf416c456740711e
Sébastien Wilmet <swilmet@gnome.org>
2018-03-09 14:21:20 +0100

    gobject-renamer: take also the namespace

--- src/gobject-renamer.sh
+++ src/gobject-renamer.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 # TODO
-# - Take also the namespace, and substitute:
-# "${old_namespace_upper}_IS_${old_classname_upper}"
-# "${old_namespace_upper}_TYPE_${old_classname_upper}"
-#
 # - Call gcu-lineup-parameters on the file if it is a *.c file.
 #
 # - Take input arguments from the command line, check the args, print usage,
@@ -12,14 +8,41 @@
 
 filename=$1
 
-old_classname_camel=Foo
+old_namespace_camel=Dh
+old_namespace_upper=$(gcu-case-converter --to-uppercase "$old_namespace_camel")
+old_namespace_lower=$(gcu-case-converter --to-lowercase "$old_namespace_camel")
+
+new_namespace_camel=Dh
+new_namespace_upper=$(gcu-case-converter --to-uppercase "$new_namespace_camel")
+new_namespace_lower=$(gcu-case-converter --to-lowercase "$new_namespace_camel")
+
+old_classname_camel=Settings
 old_classname_upper=$(gcu-case-converter --to-uppercase "$old_classname_camel")
 old_classname_lower=$(gcu-case-converter --to-lowercase "$old_classname_camel")
 
-new_classname_camel=Bar
+new_classname_camel=SettingsApp
 new_classname_upper=$(gcu-case-converter --to-uppercase "$new_classname_camel")
 new_classname_lower=$(gcu-case-converter --to-lowercase "$new_classname_camel")
 
-gcu-lineup-substitution "$old_classname_upper" "$new_classname_upper" "$filename"
-gcu-lineup-substitution "$old_classname_camel" "$new_classname_camel" "$filename"
-gcu-lineup-substitution "$old_classname_lower" "$new_classname_lower" "$filename"
+# UPPER
+gcu-lineup-substitution "${old_namespace_upper}_${old_classname_upper}" \
+			"${new_namespace_upper}_${new_classname_upper}" \
+			"$filename"
+
+gcu-lineup-substitution "${old_namespace_upper}_IS_${old_classname_upper}" \
+			"${new_namespace_upper}_IS_${new_classname_upper}" \
+			"$filename"
+
+gcu-lineup-substitution "${old_namespace_upper}_TYPE_${old_classname_upper}" \
+			"${new_namespace_upper}_TYPE_${new_classname_upper}" \
+			"$filename"
+
+# Camel
+gcu-lineup-substitution "${old_namespace_camel}${old_classname_camel}" \
+			"${new_namespace_camel}${new_classname_camel}" \
+			"$filename"
+
+# lower
+gcu-lineup-substitution "${old_namespace_lower}_${old_classname_lower}" \
+			"${new_namespace_lower}_${new_classname_lower}" \
+			"$filename"
